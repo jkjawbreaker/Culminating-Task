bf6702f51cc1d863ded717ce3c79f538cbecc143
jkjawbreaker@gmail.com <grumpy1>
2018-05-17 15:06:55 -0400

    *edit*

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,41 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import static com.company.projConstants.*;
 public class Main {
-
-    static int gridSize=0;
-    static String[] arg={null};
+    static int gridSize;
+    static int startXPos;
+    static int startYPos;
+    static int createInt(int Max,int Min){
+        boolean repeat=true;
+        int result=0;
+        do {
+            repeat=false;
+            try {
+                Scanner inputScanner = new Scanner(System.in);
+                result = inputScanner.nextInt();
+                repeat=false;
+            } catch (InputMismatchException error) {
+                System.out.println("Must be a Integer");
+                break;
+            }
+            if (repeat=false) {
+                if (Max < gridSize && gridSize < Min) {
+                    System.out.println("number in not within parameters");
+                    repeat=true;
+                } else {
+                    repeat=false;
+                }
+            }
+        }while (repeat);
+        return result;
+    }
     public static void main(String[] args) {
         mapRobot mapClass= new mapRobot();
         mineRobot mineClass= new mineRobot();
         System.out.println("How big would you like your array to be?\nmust be between 1 and 1000");
-        Scanner inputScanner = new Scanner(System.in);
-
-        try {
-            gridSize=inputScanner.nextInt();
-        }catch (InputMismatchException error){
-            System.out.println("you must enter a number");
-        }
-        if(1000<gridSize && gridSize<1){
-            System.out.print("you must enter a number between 1 and 1000");
-            System.exit(0);
-        }
+        gridSize= createInt(1000,1);
         int Grid[][]=new int[gridSize][gridSize];
-        boolean isGridFound[][]=new boolean[gridSize][gridSize];
+        boolean isGridMapped[][]=new boolean[gridSize][gridSize];
         String shownGrid[][]=new String[gridSize][gridSize];
-
+        mapClass.printGrid(shownGrid);
     }
 }
--- src/com/company/mapRobot.java
+++ src/com/company/mapRobot.java
@@ -4,6 +4,15 @@ import static com.company.projConstants.*;
  * Created by 90937kav on 15/05/2018.
  */
 public class mapRobot {
+
+    public static final void printGrid(String[][] printArray){
+        for(int i=0;i<printArray.length;i++){
+            for (int j=0;j<printArray.length;j++){
+                System.out.print(printArray[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
     private int resourceValue(){
         return INVALID;
     }
